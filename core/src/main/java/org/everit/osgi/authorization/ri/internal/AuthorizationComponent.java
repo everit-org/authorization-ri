9b07432b6eacda8f9f32702d918d8cdab8bbda7d
Balazs Zsoldos <balazs.zsoldos@everit.biz>
2014-07-30 17:27:22 +0200

    A couple of functions partially implemented

---
+++ core/src/main/java/org/everit/osgi/authorization/ri/internal/AuthorizationComponent.java
@@ -0,0 +1,121 @@
+package org.everit.osgi.authorization.ri.internal;
+
+import org.apache.felix.scr.annotations.Activate;
+import org.apache.felix.scr.annotations.Component;
+import org.apache.felix.scr.annotations.ConfigurationPolicy;
+import org.apache.felix.scr.annotations.Properties;
+import org.apache.felix.scr.annotations.Property;
+import org.apache.felix.scr.annotations.Reference;
+import org.everit.osgi.authorization.AuthorizationManager;
+import org.everit.osgi.authorization.PermissionChecker;
+import org.everit.osgi.authorization.ri.schema.qdsl.QPermission;
+import org.everit.osgi.cache.api.CacheConfiguration;
+import org.everit.osgi.cache.api.CacheFactory;
+import org.everit.osgi.cache.api.CacheHolder;
+import org.everit.osgi.querydsl.support.QuerydslSupport;
+import org.everit.osgi.transaction.helper.api.TransactionHelper;
+import org.osgi.framework.BundleContext;
+
+import com.mysema.query.sql.dml.SQLInsertClause;
+
+@Component(name = AuthorizationRIConstants.SERVICE_FACTORYPID_AUTHORIZATION, configurationFactory = true,
+        policy = ConfigurationPolicy.REQUIRE, metatype = true)
+@Properties({ @Property(name = "querydslSupport.target"), @Property(name = "transactionHelper.target"),
+        @Property(name = "cacheFactory.target", value = "(cacheName=noop)") })
+public class AuthorizationComponent implements AuthorizationManager, PermissionChecker {
+
+    @Reference(bind = "setCacheFactory")
+    private CacheFactory cacheFactory;
+
+    @Reference(bind = "setPermissionCacheConfiguration")
+    private CacheConfiguration<String, Boolean> permissionCacheConfiguration;
+
+    @Reference(bind = "setPermissionInheritanceCacheConfiguration")
+    private CacheConfiguration<Long, Long[]> permissionInheritanceCacheConfiguration;
+
+    private CacheHolder<Long, Long[]> piCacheHolder = null;
+
+    @Reference(name = "querydslSupport", bind = "setQdsl")
+    private QuerydslSupport qdsl;
+
+    @Reference(name = "transactionHelper", bind = "setTh")
+    private TransactionHelper th;
+
+    @Activate
+    public void activate(BundleContext bundleContext) {
+        piCacheHolder = cacheFactory.createCache(permissionInheritanceCacheConfiguration, this.getClass()
+                .getClassLoader());
+    }
+
+    @Override
+    public void addPermission(long authorizedResourceId, long targetResourceId, String action) {
+        th.required(() -> qdsl.execute((connection, configuration) -> {
+            QPermission p = QPermission.permission;
+            SQLInsertClause insert = new SQLInsertClause(connection, configuration, p);
+            insert.set(p.authorizedResourceId, authorizedResourceId).set(p.targetResourceId, targetResourceId)
+                    .set(p.action, action).execute();
+
+            return null;
+        }));
+    }
+
+    @Override
+    public void addPermissionInheritance(long parentResourceId, long childResourceId) {
+        th.required(() -> qdsl.execute((connection, configuration) -> {
+
+            return null;
+        }));
+    }
+
+    @Override
+    public void clearCache() {
+        // TODO Auto-generated method stub
+
+    }
+
+    @Override
+    public long[] getAuthorizationScope(long resourceId) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public boolean hasPermission(long authorizedResourceId, long targetResourceId, String action) {
+        // TODO Auto-generated method stub
+        return false;
+    }
+
+    @Override
+    public void removePermission(long authorizedResourceId, long targetResourceId, String action) {
+        // TODO Auto-generated method stub
+
+    }
+
+    @Override
+    public void removePermissionInheritance(long parentResourceId, long childResourceId) {
+        // TODO Auto-generated method stub
+
+    }
+
+    public void setCacheFactory(CacheFactory cacheFactory) {
+        this.cacheFactory = cacheFactory;
+    }
+
+    public void setPermissionCacheConfiguration(CacheConfiguration<String, Boolean> permissionCacheConfiguration) {
+        this.permissionCacheConfiguration = permissionCacheConfiguration;
+    }
+
+    public void setPermissionInheritanceCacheConfiguration(
+            CacheConfiguration<Long, Long[]> permissionInheritanceCacheConfiguration) {
+        this.permissionInheritanceCacheConfiguration = permissionInheritanceCacheConfiguration;
+    }
+
+    public void setQdsl(QuerydslSupport qdsl) {
+        this.qdsl = qdsl;
+    }
+
+    public void setTh(TransactionHelper th) {
+        this.th = th;
+    }
+
+}
---
+++ core/src/main/java/org/everit/osgi/authorization/ri/internal/AuthorizationRIConstants.java
@@ -0,0 +1,9 @@
+package org.everit.osgi.authorization.ri.internal;
+
+public final class AuthorizationRIConstants {
+
+    public static final String SERVICE_FACTORYPID_AUTHORIZATION = "org.everit.osgi.authorization.ri.Authorization";
+
+    private AuthorizationRIConstants() {
+    }
+}
